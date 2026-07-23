8c520889cea496b8c0671886af7febeba866e254
ssumiran <ssumiran@yahoo.com>
2026-07-23 19:37:44 -0400

    Set up the pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import './App.css'
 import Main from './pages/Main'
 import Currencies from './pages/Currencies'
 import Price from './pages/Price'
+import Nav from './component/nav';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/" element={<Main/>}/>
         <Route path="/currencies" element={<Currencies/>}/>
         <Route path="/price" element={<Price/>}/>
+        <Route path="/price/:symbol" element={<Price/>}/>
       </Routes>
     </div>
   )
