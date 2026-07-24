c5024a3bbf414d8dc83e0de99530413b7f31bfa3
ssumiran <ssumiran@yahoo.com>
2026-07-24 09:25:18 -0400

    Fix the react DOM using new version

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// Import BrowserRouter
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+      <BrowserRouter>
       <App />
+    </BrowserRouter>
   </StrictMode>
 )
+
+
+
+// Wrap the App component with the BrowserRouter component to enable the router features.
+// ReactDOM.render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+//   document.getElementById("root")
+// );
